5e595ad3dd74ca2a1f80116ff582087e53d43a03
Marina Permiakova <marinapermyakova13@gmail.com>
2021-03-01 15:53:52 +0200

    adds styles for mobile and desktop versions

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,6 @@
 const refs = {
   searchForm: document.querySelector('.search-form'),
   galleryList: document.querySelector('.gallery'),
-  searchBtn: document.querySelector('.search_btn'),
   loadBtn: document.querySelector('.load_btn'),
 };
 
